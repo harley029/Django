cee68c31da39cd97b993019594b33f340437de71
harley029 <harley029@gmail.com>
2024-09-21 21:46:31 +0200

    12.09.2024

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -19,20 +19,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from courses.views import CourseListView
+from django.contrib.auth import views as auth_views
+
+
+class LogoutView(auth_views.LogoutView):
+    http_method_names = ["get", "post"]  # Дозволяє використання GET-запиту
 
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(),
-          name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(),
-          name='logout'),
-    path('admin/', admin.site.urls),
-    path('course/', include('courses.urls')),
-    path('', CourseListView.as_view(), name='course_list'),
-    path('students/', include('students.urls')),
-    path('api/', include('courses.api.urls', namespace='api')),
-    path('chat/', include('chat.urls', namespace='chat')),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", LogoutView.as_view(), name="logout"),
+    path("admin/", admin.site.urls),
+    path("course/", include("courses.urls")),
+    path("", CourseListView.as_view(), name="course_list"),
+    path("students/", include("students.urls")),
+    path("api/", include("courses.api.urls", namespace="api")),
+    path("chat/", include("chat.urls", namespace="chat")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 if settings.DEBUG:
