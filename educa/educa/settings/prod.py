566ef7303901eaea1d4329212a10c08a05e54c1e
harley029 <harley029@gmail.com>
2024-09-26 09:26:48 +0200

    26.09.24

--- educa/educa/settings/prod.py
+++ educa/educa/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 ADMINS = [
     ("Oleksandr, Kh.", "harley029@gmail.com"),
 ]
-ALLOWED_HOSTS = ["educaproject.com", "www.educaproject.com"]
+ALLOWED_HOSTS = [".educaproject.com"]
 
 DATABASES = {
     "default": {
@@ -21,3 +21,8 @@ DATABASES = {
 REDIS_URL = "redis://cache:6379"
 CACHES["default"]["LOCATION"] = REDIS_URL
 CHANNEL_LAYERS["default"]["CONFIG"]["hosts"] = [REDIS_URL]
+
+# Безопасность
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
