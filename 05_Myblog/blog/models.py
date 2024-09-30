4e1a76201386a940c80de2729b34deaa28e5e71a
harley029 <harley029@gmail.com>
2024-09-30 21:49:05 +0200

    SEO URL added

--- 05_Myblog/blog/models.py
+++ 05_Myblog/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 from core.models import BaseModel
@@ -17,9 +18,7 @@ class Post(BaseModel):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
@@ -32,3 +31,9 @@ class Post(BaseModel):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
