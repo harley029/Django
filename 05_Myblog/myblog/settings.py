4c44481033f4f7efa9655c64ae0d41bd9141d1de
harley029 <harley029@gmail.com>
2024-10-04 16:56:20 +0200

    RSS added

--- 05_Myblog/myblog/settings.py
+++ 05_Myblog/myblog/settings.py
@@ -31,15 +31,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
-
 INSTALLED_FRAMEWORKS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
     "django.contrib.staticfiles",
 ]
 
