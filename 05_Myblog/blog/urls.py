543b5ac1868a4f1063cad85170203b8a57fca9b1
harley029 <harley029@gmail.com>
2024-10-06 16:10:47 +0200

    Switched to class-based views

--- 05_Myblog/blog/urls.py
+++ 05_Myblog/blog/urls.py
@@ -7,12 +7,19 @@ app_name = "blog"
 
 
 urlpatterns = [
-    path("", views.post_list, name="post_list"),
-    # path("", views.PostListView.as_view(), name="post_list"),
-    path("<int:year>/<int:month>/<int:day>/<slug:post>/", views.post_detail, name="post_detail"),
-    path('<int:post_id>/share/', views.post_share, name='post_share'),
-    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
-    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
-    path('feed/', LatestPostsFeed(), name='post_feed'),
-    path('search/', views.post_search, name='post_search'),
+    path("", views.PostListView.as_view(), name="post_list"),
+    path("<int:year>/<int:month>/<int:day>/<slug:post>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("<int:post_id>/share/", views.PostShareView.as_view(), name="post_share"),
+    path("<int:post_id>/comment/", views.PostCommentView.as_view(), name="post_comment"),
+    path("tag/<slug:tag_slug>/", views.PostListView.as_view(), name="post_list_by_tag"),
+    path("search/", views.PostSearchView.as_view(), name="post_search"),
+    path("feed/", LatestPostsFeed(), name="post_feed"),
+    # ----------------- function-based views --------------------------------
+    # path("", views.post_list, name="post_list"),
+    # path("<int:year>/<int:month>/<int:day>/<slug:post>/", views.post_detail, name="post_detail"),
+    # path("<int:post_id>/share/", views.post_share, name="post_share"),
+    # path("<int:post_id>/comment/", views.post_comment, name="post_comment"),
+    # path("tag/<slug:tag_slug>/", views.post_list, name="post_list_by_tag"),
+    # path("feed/", LatestPostsFeed(), name="post_feed"),
+    # path("search/", views.post_search, name="post_search"),
 ]
--- 05_Myblog/blog/views.py
+++ 05_Myblog/blog/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView # for class views
+from django.views.generic import ListView, DetailView, FormView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count
@@ -12,6 +12,181 @@ from blog.models import Post
 from blog.forms import EmailPostForm, CommentForm, SearchForm
 
 
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post/list.html"
+    context_object_name = "posts"
+    paginate_by = 3  # Кількість постів на сторінку
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get("tag_slug")
+        queryset = Post.published.all()
+        if tag_slug:
+            tag = get_object_or_404(Tag, slug=tag_slug)
+            queryset = queryset.filter(tags__in=[tag])
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_slug = self.kwargs.get("tag_slug")
+        if tag_slug:
+            context["tag"] = get_object_or_404(Tag, slug=tag_slug)
+        else:
+            context["tag"] = None
+        return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post/detail.html"
+    context_object_name = "post"
+
+    def get_object(self):
+        year = self.kwargs.get("year")
+        month = self.kwargs.get("month")
+        day = self.kwargs.get("day")
+        slug = self.kwargs.get("post")
+        return get_object_or_404(
+            Post,
+            status=Post.Status.PUBLISHED,
+            slug=slug,
+            publish__year=year,
+            publish__month=month,
+            publish__day=day,
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object
+
+        # Активні коментарі
+        comments = post.comments.filter(active=True)
+
+        # Форма для додавання коментаря
+        form = CommentForm()
+
+        # Пошук схожих постів за тегами
+        post_tags_ids = post.tags.values_list("id", flat=True)
+        similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(
+            id=post.id
+        )
+        similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+            "-same_tags", "-publish"
+        )[:4]
+
+        context["comments"] = comments
+        context["form"] = form
+        context["similar_posts"] = similar_posts
+        return context
+
+
+class PostShareView(FormView):
+    template_name = "blog/post/share.html"
+    form_class = EmailPostForm
+
+    def dispatch(self, request, *args, **kwargs):
+        self.post_instance = get_object_or_404(
+            Post, id=kwargs["post_id"], status=Post.Status.PUBLISHED
+        )
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        # Якщо форма валидна, отправляем email
+        cd = form.cleaned_data
+        post_url = self.request.build_absolute_uri(
+            self.post_instance.get_absolute_url()
+        )
+        subject = f"{cd['name']} ({cd['email']}) recommends you read {self.post_instance.title}"
+        message = f"Read {self.post_instance.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}"
+        send_mail(subject, message, from_email=None, recipient_list=[cd["to"]])
+        return self.render_to_response(self.get_context_data(form=form, sent=True))
+
+    def get_context_data(self, **kwargs):
+        # Добавляем объект поста в контекст
+        context = super().get_context_data(**kwargs)
+        context["post"] = self.post_instance
+        return context
+
+
+class PostCommentView(FormView):
+    template_name = "blog/post/comment.html"
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        # Отримуємо пост, до якого додається коментар
+        self.post_instance = get_object_or_404(
+            Post, id=kwargs["post_id"], status=Post.Status.PUBLISHED
+        )
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        # Якщо форма валідна, створюємо новий коментар без збереження в базі
+        comment = form.save(commit=False)
+        # Прив'язуємо коментар до поста
+        comment.post = self.post_instance
+        # Зберігаємо коментар в базі даних
+        comment.save()
+
+        # Повертаємо контекст з постом і коментарем
+        return self.render_to_response(
+            self.get_context_data(form=form, comment=comment)
+        )
+
+    def get_context_data(self, **kwargs):
+        # Додаємо пост і форму в контекст
+        context = super().get_context_data(**kwargs)
+        context["post"] = self.post_instance
+        context["comment"] = kwargs.get("comment")
+        return context
+
+
+class PostSearchView(FormView):
+    template_name = "blog/post/search.html"
+    form_class = SearchForm
+
+    def get(self, request):
+        if "query" in request.GET:
+            form = self.form_class(request.GET)
+            if form.is_valid():
+                return self.form_valid(form)
+            else:
+                return self.form_invalid(form)
+        else:
+            return self.render_to_response(
+                self.get_context_data(form=self.form_class())
+            )
+
+    def form_valid(self, form):
+        # Отримуємо запит із форми після валідації
+        query = form.cleaned_data["query"]
+        # Створюємо пошукові вектори для заголовку та тіла поста з вагами
+        search_vector = SearchVector(
+            "title", weight="A", config="russian"
+        ) + SearchVector("body", weight="B", config="russian")
+        # Створюємо пошуковий запит
+        search_query = SearchQuery(query, config="russian")
+        # Виконуємо пошук і сортуємо результати за релевантністю
+        results = (
+            Post.published.annotate(
+                search=search_vector, rank=SearchRank(search_vector, search_query)
+            )
+            .filter(rank__gte=0.3)
+            .order_by("-rank")
+        )
+        # Повертаємо відповідь із результатами пошуку
+        return self.render_to_response(
+            self.get_context_data(form=form, query=query, results=results)
+        )
+
+    def get_context_data(self, **kwargs):
+        # Додаємо пошуковий запит та результати в контекст
+        context = super().get_context_data(**kwargs)
+        context["query"] = kwargs.get("query", None)
+        context["results"] = kwargs.get("results", [])
+        return context
+
+
+# ----------------- function views --------------------------------
 def post_list(request, tag_slug=None):
     # posts = Post.published.all() - was without pagination
 
@@ -141,11 +316,3 @@ def post_search(request):
         "blog/post/search.html",
         {"form": form, "query": query, "results": results},
     )
-
-
-# -------------- class views ----------------
-# class PostListView(ListView):
-#     queryset = Post.published.all()
-#     context_object_name = 'posts'
-#     paginate_by = 3
-#     template_name = 'blog/post/list.html'
